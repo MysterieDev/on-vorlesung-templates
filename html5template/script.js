43af04f2bcffd172ae732ca8badcf17f1c065606
josef <josefsle97@gmail.com>
2022-03-21 14:37:01 +0100

    add folder

--- html5template/script.js
+++ html5template/script.js
@@ -1,1 +1,25 @@
-console.log("20" * 2)
+const anredeM = "Herr ";
+const anredeW = "Frau ";
+
+function returnGreeting(personName, isAnredeM){
+
+    function makeName(personName, isAnredeM){
+        if(isAnredeM){
+            return "Hello " + anredeM + personName;
+        }
+        else{
+            return "Hello " + anredeW + personName;
+        }
+    }
+
+    return makeName(personName, isAnredeM);
+}
+
+// loops until parameter reaches value 10
+function loopUntil10(x) {
+    if (x >= 10){
+      return;
+    }
+    console.log(x);
+    loopUntil10(x + 1);
+  }
--- object_arrays_iterations/script.js
+++ object_arrays_iterations/script.js
@@ -2,14 +2,14 @@
 const employee = {
     vorname: 'josef',
     nachname: 'test',
-    rolle: 'azubi',
+    rolle: 'geselle',
     rechtelevel: 1,
 }
 
 showEmployee(employee);
 
 
-const todos = ["abwasch", "bei der vorlesung nichts zocken", "inta abschalten", "js lernen"]
+const todos = ["abwasch", "bei der vorlesung nichts zocken", "insta abschalten", "js lernen"]
 const moreWeirdTodos = [23.75, true, false, [1, 2], {a: "one", b: 'two'}]
 
 showTodos(todos, 'todos');

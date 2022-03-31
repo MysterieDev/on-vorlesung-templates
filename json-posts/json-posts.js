ad9a79e22ae0f6c939cdbb6a078196669825b643
josef <josefsle97@gmail.com>
2022-03-31 14:21:36 +0200

    step1421

--- json-posts/json-posts.js
+++ json-posts/json-posts.js
@@ -13,9 +13,10 @@ function addPostsToSite(){
             newPost.innerHTML = `
             <div class="ui card">
                 <div class="content">
-                    <div class="header"></div>
+                    <div class="header">${post.title.substr(0,15)}</div>
                     <div class="description">
-                        <button class="ui secondary button">read more</button>
+                       ${post.body.substr(0,60)}..
+                        <br/><button class="ui secondary button">read more</button>
                     </div>
                 </div>
             </div>
@@ -24,5 +25,31 @@ function addPostsToSite(){
             postContainer.appendChild(newPost);
         } 
     });
+}
 
+function makeThreeColumns(){
+    if(postContainer.classList.contains('two')){
+        postContainer.classList.replace('two', 'three')
+    }
+    else{
+        postContainer.classList.replace('one', 'three')
+    }
+}
+
+function makeTwoColumns(){
+    if(postContainer.classList.contains('three')){
+        postContainer.classList.replace('three', 'two')
+    }
+    else{
+        postContainer.classList.replace('one', 'two')
+    }
+}
+
+function makeOneColumns(){
+    if(postContainer.classList.contains('three')){
+        postContainer.classList.replace('three', 'one');
+    }
+    else if(postContainer.classList.contains('two')){
+        postContainer.classList.replace('two', 'one');
+    }
 }
